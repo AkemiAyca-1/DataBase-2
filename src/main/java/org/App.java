bcd65173ab60cee5a4f292eafad43ee0dea732d4
SergioMainJG <arce.roldan.sergio@gmail.com>
2026-03-29 21:08:53 -0600

    app with menu added

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -1,7 +1,10 @@
 package org;
 
+import org.ui.Menu;
+
 public class App {
     public static void main( String[] args ){
-        System.out.println( "Hello World!" );
+        var menu = new Menu();
+        menu.start();
     }
 }
