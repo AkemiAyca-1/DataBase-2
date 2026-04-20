7001fa3482c16e6a5105dfbef9714594d1783afb
SergioMainJG <arce.roldan.sergio@gmail.com>
2026-04-19 23:54:57 -0600

    Login fixed. Password will be safe and hiden if the program is running on terminal, not working in IntellijIDEA Services (works in IntellijIDEA integrated terminal)

--- src/main/java/org/utils/SecurePasswordReader.java
+++ src/main/java/org/utils/SecurePasswordReader.java
@@ -1,6 +1,7 @@
 package org.utils;
 
 import java.io.Console;
+import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class SecurePasswordReader {
@@ -8,28 +9,45 @@ public class SecurePasswordReader {
     public static String readPassword(String prompt) {
         Console console = System.console();
         if (console != null) {
-            char[] password = console.readPassword(prompt);
-            return new String(password);
-        } else {
+            char[] password = console.readPassword(prompt + " ");
+            return password != null ? new String(password) : "";
+        }
+
+        try {
+            Process sttyOff = Runtime.getRuntime()
+                    .exec(new String[]{"sh", "-c", "stty -echo < /dev/tty"});
+            sttyOff.waitFor();
+
+            System.out.print(prompt + " ");
+            Scanner tty = new Scanner(new FileInputStream("/dev/tty"));
+            String pwd = tty.nextLine();
+
+            Process sttyOn = Runtime.getRuntime()
+                    .exec(new String[]{"sh", "-c", "stty echo < /dev/tty"});
+            sttyOn.waitFor();
+            System.out.println();
+
+            return pwd;
+        } catch (Exception e) {
             System.out.print(prompt + " (visible - ejecuta desde terminal para ocultarla): ");
-            Scanner scanner = new Scanner(System.in);
-            return scanner.nextLine();
+            return new Scanner(System.in).nextLine();
         }
     }
 
     public static boolean isPasswordSecure(String password) {
         if (password == null || password.length() < 6) return false;
 
-        int uppercase = 0, lowercase = 0, special = 0;
+        int uppercase = 0, lowercase = 0, special = 0, numbers = 0;
         String specialChars = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
 
         for (char c : password.toCharArray()) {
             if (Character.isUpperCase(c))          uppercase++;
             else if (Character.isLowerCase(c))     lowercase++;
             else if (specialChars.indexOf(c) >= 0) special++;
+            else if (Character.isDigit(c)) numbers++;
         }
 
-        return uppercase >= 2 && lowercase >= 2 && special >= 2;
+        return uppercase >= 2 && lowercase >= 2 && special >= 2 && numbers >= 2;
     }
 
     public static boolean isEmailValid(String email) {
